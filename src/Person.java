fa412cc3f7a8d3cf956cce7c491679d6190a6b7d
Tiffani <tiffani.marie.watson@gmail.com>
2023-06-20 14:00:10 -0500

    Exercise 1

---
+++ src/Person.java
@@ -0,0 +1,32 @@
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello, my name is " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        // Create a new Person object
+        Person person = new Person("John Doe");
+
+        // Test the methods
+        System.out.println("Initial name: " + person.getName());
+
+        person.setName("Jane Smith");
+        System.out.println("New name: " + person.getName());
+
+        person.sayHello();
+    }
+}
