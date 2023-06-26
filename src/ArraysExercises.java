fc857df95d6de7688d8098fd18437a2bb47c493d
Tiffani <tiffani.marie.watson@gmail.com>
2023-06-26 11:03:37 -0500

    finished second task for problem 1

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -5,7 +5,17 @@ public class ArraysExercises {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(numbers);
         // This outputs: [I@<some hexadecimal value>
-        // This doesn't display the contents of the array elements that I initiall expected.
+        // This doesn't display the contents of the array elements that I initial expected.
 
+        // Below is the code for the second task for problem 1
+        Person[] persons = new Person[3];
+        persons[0] = new Person("John");
+        persons[1] = new Person("Jane");
+        persons[2] = new Person("Alice");
+
+        for (Person person : persons) {
+            System.out.println(person.getName());
+        }
     }
 }
+
