588854d57ddbd1dcf6cfbd989f794f7450a40147
Tiffani <tiffani.marie.watson@gmail.com>
2023-06-28 14:40:18 -0500

    Changed my existing Rectangle class to inherit from Quadrilateral and implement Measurable

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,3 +1,28 @@
 package shapes;
 
+public class Rectangle extends Quadrilateral implements Measurable {
 
+    public Rectangle(double length, double width) {
+        super(length, width);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * width + 2 * length;
+    }
+
+    @Override
+    public double getArea() {
+        return width * length;
+    }
+
+    @Override
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public void setWidth(double width) {
+        this.width = width;
+    }
+}
