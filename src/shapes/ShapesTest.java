201d3be12e09f1617e6fb6e736e0551ad55d5382
Tiffani <tiffani.marie.watson@gmail.com>
2023-06-28 14:44:01 -0500

    update a few classes

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,17 +1,19 @@
 package shapes;
 
 public class ShapesTest {
-    public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(5, 4);
-        System.out.println("Rectangle - Box 1:");
-        System.out.println("Perimeter: " + box1.getPerimeter());
-        System.out.println("Area: " + box1.getArea());
-        System.out.println();
 
-        Rectangle box2 = new Square(5);
-        System.out.println("Square - Box 2:");
-        System.out.println("Perimeter: " + box2.getPerimeter());
-        System.out.println("Area: " + box2.getArea());
-        System.out.println();
+    public static void main(String[] args) {
+
+        Measurable box1 = new Rectangle(5, 4);
+
+        System.out.println("box1.getArea() = " + box1.getArea());
+        System.out.println("box1.getPerimeter() = " + box1.getPerimeter());
+//        System.out.println("box1.getLength() = " + box1.getLength()); // only when box1 is a quadrilateral or class that extends from such
+
+
+        box1 = new Square(5);
+
+        System.out.println("box1.getArea() = " + box1.getArea());
+        System.out.println("box1.getPerimeter() = " + box1.getPerimeter());
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,2 +1,30 @@
 package shapes;
 
+public class Square extends Quadrilateral {
+
+    public Square(double length) {
+        super(length, length);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * length;
+    }
+
+    @Override
+    public double getArea() {
+        return length * length;
+    }
+
+    @Override
+    public void setLength(double length) {
+        this.length = length;
+        width = length;
+    }
+
+    @Override
+    public void setWidth(double width) {
+        this.width = width;
+        length = width;
+    }
+}
